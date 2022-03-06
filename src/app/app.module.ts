3dd7e20c76061a53202aef52332c3bdf82a370ad
kiyimba1 <edwineddie54@gmail.com>
2022-03-06 15:15:36 +0300

    Added a common module for all material importd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'
+
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HeaderComponent
   ],
   imports: [
-    BrowserModule,
+
     AppRoutingModule,
-    BrowserAnimationsModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
